092261ed592712c1ab3f30e9e9758125288a3d16
ldcqkqn <40484355+ldcqkqn@users.noreply.github.com>
2025-08-24 23:39:17 +0900

    로딩 속도에 따라 실패율이 있어 명시적으로 처음 페이지가 로딩될때까지 10초간 기다리도록 내용 추가함

--- tests/test_google.py
+++ tests/test_google.py
@@ -1,11 +1,15 @@
-from pages.google_page import GooglePage
 # assert를 사용하는 이유는 구문 간결 및 에러 내용 및 CI/CD에 필요하다.
+from pages.google_page import GooglePage
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 def test_google_selenium(driver):
     google = GooglePage(driver)
     google.open()
     google.search("Selenium")
-    assert google.title_contains("Selenium") 
+    # 페이지가 완전히 로드될 때까지 기다리기 위해
+    WebDriverWait(driver, 10).until(EC.url_contains("Selenium"))
+    assert google.title_contains("Selenium")
 
 def test_google_search_pytyon(driver):
     google = GooglePage(driver)
