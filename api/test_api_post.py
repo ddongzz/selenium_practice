70d99171d0754541643ca7c18fb849ac6652b4e0
ldcqkqn <40484355+ldcqkqn@users.noreply.github.com>
2026-04-15 00:27:44 +0900

    post 기본 내용 추가

---
+++ api/test_api_post.py
@@ -0,0 +1,30 @@
+import requests
+import pytest
+
+def test_create_new_user():
+    print("API POST 테스트 시작")
+
+    url = "https://reqres.in/api/users"
+
+    payload = {
+        "name": "Dongchan Lee",
+        "job": "QA Engineer"
+    }
+
+    headers = {
+        "x-api-key": "pub_46acecc3066bc0da2b0f3f19aa63d77ad99e1bfb052def73453ce1af06382153"
+    }
+
+    response = requests.post(url, json=payload, headers=headers)
+
+    assert response.status_code == 201, f"데이터 생성 실패 : {response.status_code}"
+    print("데이터 생성 201 성공")
+
+    data = response.json()
+    print(f"서버 응답 데이터: {data}")
+
+    assert data["name"] == "Dongchan Lee", "이름이 같지 않습니다."
+    assert data["job"] == "QA Engineer", "직업이 같지 않습니다."
+    assert "id" in data, "고유 ID가 발급되지 않았습니다."
+
+    print(f"서버가 부여한 ID : {data['id']}")
